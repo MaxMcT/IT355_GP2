93c17ab69cc33b0954ed16764c1ad8c7315d2263
Zach Contreras <158154908+Zach729@users.noreply.github.com>
2024-11-17 20:34:41 -0600

    CWE-190
    Updated genrateID() to check for overflow before assigning the generated ID

--- Student.cpp
+++ Student.cpp
@@ -82,7 +82,12 @@ public:
 	random_device rd;
         mt19937 generator(rd());
         uniform_int_distribution<int> distrubtion(1000000, 9999999);
-        return distrubtion(generator);
+        int id = distribution(generator);
+    	if (id < 0) {  // Check for overflow
+        cout << "Error: Generated ID caused overflow.\n";
+        return 1000000;  // Default ID
+    	}
+    	return id;
     }
 
     double calculateGPA() {
